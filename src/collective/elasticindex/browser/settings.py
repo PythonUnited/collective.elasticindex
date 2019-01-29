dde58f3f7a55d6b1c9bfe87a1159eba3a8c0f99f
Kim Chee Leong <leong@turftorr.nl>
2019-01-29 16:17:56 +0100

    Update module for Plone 5 compability

--- src/collective/elasticindex/browser/settings.py
+++ src/collective/elasticindex/browser/settings.py
@@ -1,9 +1,14 @@
 
 from Products.statusmessages.interfaces import IStatusMessage
-from plone.app.controlpanel.form import ControlPanelForm
-from zope.formlib import form
+# from plone.app.controlpanel.form import ControlPanelForm
+# from zope.formlib import form
 from zope.i18nmessageid import MessageFactory
+from zope import schema
 
+from plone.app.registry.browser.controlpanel import ControlPanelFormWrapper
+from plone.app.registry.browser.controlpanel import RegistryEditForm
+from plone.directives import form
+from z3c.form import field, button
 from collective.elasticindex.interfaces import IElasticSettings
 from collective.elasticindex.changes import changes
 from collective.elasticindex.utils import create_index, delete_index
@@ -12,40 +17,37 @@ from collective.elasticindex.utils import create_index, delete_index
 _ = MessageFactory('collective.elasticindex')
 
 
-class ElasticIndexSettings(ControlPanelForm):
-    label = _("Elasticsearch configuration")
-    description = _("Configure elasticsearch index.")
-    form_name = _("Elasticsearch configuration")
-    form_fields = form.Fields(IElasticSettings)
+class SettingsEditForm(RegistryEditForm):
+    schema = IElasticSettings
+    label = u"Elasticsearch settings"
 
-    @form.action('Create index', name='create_index')
-    def create_index(self, action, data):
-        send = IStatusMessage(self.request).add
-        try:
-            create_index(IElasticSettings(self.context))
-        except:
-            send("Error while creating the index.", type='error')
-        else:
-            send("Index created.")
+    fields = field.Fields(IElasticSettings)
 
-    @form.action('Delete index', name='delete_index')
-    def delete_index(self, action, data):
-        send = IStatusMessage(self.request).add
-        try:
-            delete_index(IElasticSettings(self.context))
-        except:
-            send("Error while deleting the index.", type='error')
-        else:
-            send("Index deleted.")
+    @button.buttonAndHandler(_(u"Save"), name='save')
+    def handleSave(self, action):
+        super(SettingsEditForm, self).handleSave(self, action)
 
-    @form.action('Import site content', name='import_content')
-    def import_site_content(self, action, data):
-        send = IStatusMessage(self.request).add
-        try:
-            changes.verify_and_index_container(self.context)
-        except:
-            send("Error while indexing the index.", type='error')
-        else:
-            send("Index refreshed.")
+    @button.buttonAndHandler(_(u"Cancel"), name='cancel')
+    def handleCancel(self, action):
+        super(SettingsEditForm, self).handleCancel(self, action)
 
-    actions += form.Actions(*list(ControlPanelForm.actions))
+    @button.buttonAndHandler(_(u"Create index"), name='create_index')
+    def handle_create_index(self, action):
+        create_index(IElasticSettings(self.context))
+        IStatusMessage(self.request).add("Index created.")
+
+    @button.buttonAndHandler(_(u"Delete index"), name='delete_index')
+    def handle_delete_index(self, action):
+        delete_index(IElasticSettings(self.context))
+        IStatusMessage(self.request).add("Index deleted.")
+
+    @button.buttonAndHandler(_(u"Import site content"), name='import_content')
+    def handle_import_content(self, action):
+        changes.verify_and_index_container(self.context)
+        IStatusMessage(self.request).add("Index refreshed.")
+
+
+class SettingsView(ControlPanelFormWrapper):
+    form = SettingsEditForm
+
+
--- src/collective/elasticindex/interfaces.py
+++ src/collective/elasticindex/interfaces.py
@@ -9,37 +9,44 @@ _ = MessageFactory('collective.elasticindex')
 class IElasticSettings(Interface):
     only_published = schema.Bool(
         title=_(u'Index only published content?'),
-        default=True)
+        default=True
+    )
     index_security = schema.Bool(
         title=_(u'Index security?'),
-        default=False)
+        default=False
+    )
     normalize_domain_name = schema.TextLine(
         title=_(u"Normalize domain name"),
         description=_(u"If specified, replace the domain name in documents "
                       u"URL with this one."),
-        required=False)
+        required=False
+    )
     index_name = schema.TextLine(
         title=_(u"Index name"),
         description=_(u"Elastic-Search index name where to index and search "
                       u"for documents."),
-        required=True)
+        required=True
+    )
     server_urls = schema.List(
         title=_(u"Server URLs"),
         description=_(u"URLs to contact Elastic-Search servers"),
         value_type=schema.URI(),
-        required=True)
+        required=True
+    )
     public_server_urls = schema.List(
         title=_(u"Server URLs to use for the public search"),
         description=_(u"URLs for the public search to contact Elastic-Search. "
                       u"If not specified regular server URLs will be used."),
         value_type=schema.URI(),
-        required=True)
+        required=False
+    )
     public_through_plone = schema.Bool(
         title=_(u"Proxy search requests through Plone and apply "
                 u"security filter?"),
         description=_(u"Check this and index security if you want to "
                       u"have a private search"),
-        default=False)
+        default=False
+    )
 
     def get_search_urls():
         pass
